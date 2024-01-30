12f485c1a740fa2dc251aade56d42bc0830554e8
Arheee <justinewallet1@gmail.com>
2024-01-30 16:36:51 +0100

    Exercice 2 en cours de construction :poop:

---
+++ src/main/java/fr/arheee/cinemalegrandcda/acteur/ActeurRepository.java
@@ -0,0 +1,7 @@
+package fr.arheee.cinemalegrandcda.acteur;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActeurRepository extends JpaRepository<Acteur,Integer> {
+
+}
---
+++ src/main/java/fr/arheee/cinemalegrandcda/realisateur/RealisateurRepository.java
@@ -0,0 +1,6 @@
+package fr.arheee.cinemalegrandcda.realisateur;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RealisateurRepository extends JpaRepository<Realisateur,Integer> {
+}
